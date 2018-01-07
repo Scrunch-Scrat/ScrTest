632ee4c0cc56890977d5a6979af5478b0892f5f7
Scrunch-Scrat <Vincenzo@scrunch.co.za>
2018-01-07 21:53:02 +0200

    removed practice code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ServerCompponent } from './server/server.component';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerCompponent,
-    ServersComponent
   ],
   imports: [
     HttpModule,
